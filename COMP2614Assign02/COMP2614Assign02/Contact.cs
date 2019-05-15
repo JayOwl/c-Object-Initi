122511bfa86ce696ec01775178531b7fe97a0002
Joel Benoit <joel.benoit@tsunamisolutions.com>
2019-05-15 14:58:45 -0700

    Add contact.cs to src control.

---
+++ COMP2614Assign02/COMP2614Assign02/Contact.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Assign02
+{
+    class Contact
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string province;
+        private string postalCode;
+
+        public Contact()
+        {
+        }
+
+        public Contact(string firstName, string lastName, string address, string city, string province, string postalCode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            Province = province;
+            PostalCode = postalCode;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string Province
+        {
+            get { return province; }
+            set { province = value; }
+        }
+
+        public string PostalCode
+        {
+            get { return postalCode; }
+            set { postalCode = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("LastName: {0} FirstName {1}", lastName, firstName);
+        }
+
+    }
+}
--- COMP2614Assign02/COMP2614Assign02/Program.cs
+++ COMP2614Assign02/COMP2614Assign02/Program.cs
@@ -10,6 +10,54 @@ namespace COMP2614Assign02
     {
         static void Main(string[] args)
         {
+
+            string firstName;
+            string lastName;
+            string address;
+            string city;
+            string province;
+            string postalCode;
+
+            Console.Title = "COMP2614 - Assignment 2 - A00838629";
+
+
+            System.Console.Write("Whats your first name?: ");
+            firstName = System.Console.ReadLine();
+
+            System.Console.Write("Whats your last name?: ");
+            lastName = System.Console.ReadLine();
+
+
+            System.Console.Write("Whats your address?: ");
+            address = System.Console.ReadLine();
+
+            System.Console.Write("Whats your city?: ");
+            city = System.Console.ReadLine();
+
+            System.Console.Write("Whats your province?: ");
+            province = System.Console.ReadLine();
+
+            System.Console.Write("Whats your postal code?: ");
+            postalCode = System.Console.ReadLine();
+
+            //paramter
+            Contact[] member =
+            {
+                new Contact(firstName, lastName, address, city, province, postalCode )
+            };
+
+            ConsolePrinter.PrintMembers(member);
+            //object init
+            Contact member1 =
+                 new Contact() { FirstName = firstName, LastName = lastName, Address = address, City = city, Province = province, PostalCode = postalCode };
+
+            Contact member3 = new Contact();
+            member3.FirstName = firstName;
+            member3.LastName = lastName;
+            member3.Address = address;
+            member3.City = city;
+            member3.Province = province;
+            member3.PostalCode = postalCode;
         }
     }
 }
